ea26a564220df0d265366d37e909415c88cc5578
Radev <d.radevs@abv.bg>
2018-03-21 17:01:01 +0100

    Updated Info
    Added Node JS to tab menus and to the info bar as an info

--- scripts/application.js
+++ scripts/application.js
@@ -53,61 +53,63 @@ function effects() {
                     $('.w3-progressbar:eq(0)').animate({'width': '95%'}, 1550);
                     executePercents(80, 'js');
                     $('.w3-progressbar:eq(1)').animate({'width': '80%'}, 1340);
+                    executePercents(60, 'node');
+                    $('.w3-progressbar:eq(2)').animate({'width': '60%'}, 1050);
                     executePercents(40, 'csh');
-                    $('.w3-progressbar:eq(2)').animate({'width': '40%'}, 850);
+                    $('.w3-progressbar:eq(3)').animate({'width': '40%'}, 850);
                     executePercents(30, 'cpp');
-                    $('.w3-progressbar:eq(3)').animate({'width': '30%'}, 650);
+                    $('.w3-progressbar:eq(4)').animate({'width': '30%'}, 650);
                 }, 250);
             }else if(obj === 'tabTwo') {
                 setTimeout(function () {
                     executePercents(95, 'ws');
-                    $('.w3-progressbar:eq(4)').animate({'width': '95%'}, 1550);
-                    executePercents(95, 'vs');
                     $('.w3-progressbar:eq(5)').animate({'width': '95%'}, 1550);
-                    executePercents(95, 'dw');
+                    executePercents(95, 'vs');
                     $('.w3-progressbar:eq(6)').animate({'width': '95%'}, 1550);
+                    executePercents(95, 'dw');
+                    $('.w3-progressbar:eq(7)').animate({'width': '95%'}, 1550);
                     executePercents(75, 'st');
-                    $('.w3-progressbar:eq(7)').animate({'width': '75%'}, 1300);
+                    $('.w3-progressbar:eq(8)').animate({'width': '75%'}, 1300);
                 }, 250);
             }else if(obj === 'tabThree') {
                 setTimeout(function () {
                     executePercents(90, 'aps');
-                    $('.w3-progressbar:eq(8)').animate({'width': '90%'}, 1450);
+                    $('.w3-progressbar:eq(9)').animate({'width': '90%'}, 1450);
                     executePercents(75, 'ail');
-                    $('.w3-progressbar:eq(9)').animate({'width': '75%'}, 1300);
-                    executePercents(75, 'ain');
                     $('.w3-progressbar:eq(10)').animate({'width': '75%'}, 1300);
+                    executePercents(75, 'ain');
+                    $('.w3-progressbar:eq(11)').animate({'width': '75%'}, 1300);
                     executePercents(90, 'cdw');
-                    $('.w3-progressbar:eq(11)').animate({'width': '90%'}, 1450);
+                    $('.w3-progressbar:eq(12)').animate({'width': '90%'}, 1450);
                 }, 250);
             }else if(obj === 'tabFour') {
                 setTimeout(function () {
                     executePercents(95, 'mo');
-                    $('.w3-progressbar:eq(12)').animate({'width': '95%'}, 1550);
+                    $('.w3-progressbar:eq(13)').animate({'width': '95%'}, 1550);
                     executePercents(80, 'mdp');
-                    $('.w3-progressbar:eq(13)').animate({'width': '80%'}, 1340);
-                    executePercents(80, 'mtrz');
                     $('.w3-progressbar:eq(14)').animate({'width': '80%'}, 1340);
+                    executePercents(80, 'mtrz');
+                    $('.w3-progressbar:eq(15)').animate({'width': '80%'}, 1340);
                     executePercents(85, 'mtrd');
-                    $('.w3-progressbar:eq(15)').animate({'width': '85%'}, 1360);
+                    $('.w3-progressbar:eq(16)').animate({'width': '85%'}, 1360);
                     executePercents(95, 'trdbg');
-                    $('.w3-progressbar:eq(16)').animate({'width': '95%'}, 1550);
+                    $('.w3-progressbar:eq(17)').animate({'width': '95%'}, 1550);
                 }, 250);
             }else if(obj === 'tabFive') {
                 setTimeout(function () {
                     executePercents(50, 'apmr');
-                    $('.w3-progressbar:eq(17)').animate({'width': '50%'}, 900);
+                    $('.w3-progressbar:eq(18)').animate({'width': '50%'}, 900);
                     executePercents(40, 'aae');
-                    $('.w3-progressbar:eq(18)').animate({'width': '40%'}, 800);
+                    $('.w3-progressbar:eq(19)').animate({'width': '40%'}, 800);
                     executePercents(35, 'sove');
-                    $('.w3-progressbar:eq(19)').animate({'width': '35%'}, 700);
+                    $('.w3-progressbar:eq(20)').animate({'width': '35%'}, 700);
                 }, 200);
             }else if(obj === 'tabSix') {
                 setTimeout(function () {
                     executePercents(95, 'fl');
-                    $('.w3-progressbar:eq(20)').animate({'width': '95%'}, 1550);
+                    $('.w3-progressbar:eq(21)').animate({'width': '95%'}, 1550);
                     executePercents(50, 'cu');
-                    $('.w3-progressbar:eq(21)').animate({'width': '50%'}, 900);
+                    $('.w3-progressbar:eq(22)').animate({'width': '50%'}, 900);
                 }, 200);
             }
         }
